9d54d8d47aeabd404df0a958aabe42b4c2ea14df
San Anselmo Press <info@sananselmopress.com>
2026-06-14 23:14:35 +0800

    build: remove cloudflare adapter to build static site for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,11 @@ import partytown from '@astrojs/partytown';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 import compress from 'astro-compress';
-import cloudflare from '@astrojs/cloudflare';
 import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
   site: 'https://san-anselmo.github.io',
   base: '/publications-ph',
-  adapter: cloudflare(),
   integrations: [
     sitemap(),
     partytown({
